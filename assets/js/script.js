0bf9835666d6a02c7799752a81a4f6e02ea4e52e
devNigel <nigeljoy2020@gmail.com>
2020-04-26 23:59:18 +0530

    Updates
    Added blog section, updated info

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,10 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   }
+
+
+  feedURL='https://blog.nigeljoy.me/feeds/posts/summary?published&alt=json-in-script&callback=blogPostsFeed&max-results=4';
+  loadBlogFeed(feedURL);
 });
 
 // Smooth Anchor Scrolling
@@ -44,4 +48,62 @@ function scrollFunction() {
   } else {
     document.getElementById("toTop").style.display = "none";
   }
+}
+
+
+function loadBlogFeed(file) {
+  // DOM: Create the script element
+  var jsElm = document.createElement("script");
+  // set the type attribute
+  jsElm.type = "text/javascript";
+  // make the script element load file
+  jsElm.src = file;
+  // finally insert the element to the body element in order to load the script
+  document.body.appendChild(jsElm);
+}
+
+function blogPostsFeed(ps)
+ {
+
+posts=ps.feed.entry?ps.feed.entry:null;
+
+if(posts){
+    var podiv = document.getElementsByClassName( 'posts-outer' )[0];
+    var bpdiv = document.createElement( 'div' );
+    bpdiv.setAttribute("id", "blog-posts");
+    bpdiv.setAttribute("class", "blog-posts");
+    // podiv.parentNode.insertBefore( bpdiv, podiv.nextSibling );
+    podiv.appendChild(bpdiv);
+
+    
+    posts.forEach(function(post){
+     var bpdivpost = document.createElement( 'article' );
+     bpdivpost.setAttribute("id", "blog-post");
+     bpdivpost.className = "blog-post";
+    
+     var bpdivpostCard= document.createElement( 'div' );
+     bpdivpostCard.className = "card";
+
+     var bpdivpostCardContent= document.createElement( 'div' );
+     bpdivpostCardContent.className = "card-content";
+
+     var bpdivpostTitle= document.createElement( 'h4' );
+     bpdivpostTitle.className = "blog-post-title title";
+    
+     bpdivpostTitleLink=document.createElement( 'a' );
+     bpdivpostTitleLink.setAttribute("href",post.link[4].href);
+     bpdivpostTitleLink.setAttribute("target","_blank");
+     bpdivpostTitleLink.innerHTML=post.title.$t;
+    
+     bpdivpostTitle.appendChild(bpdivpostTitleLink);
+     
+     bpdivpostCardContent.appendChild(bpdivpostTitle);
+     bpdivpostCard.appendChild(bpdivpostCardContent);
+     bpdivpost.appendChild(bpdivpostCard);
+    
+     bpdiv.appendChild(bpdivpost);
+    }); 
+    
+}
+
 }
